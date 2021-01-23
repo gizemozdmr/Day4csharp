fa9b8ad3a903a75a436fb0406c104c8d87a15248
Gizem Özdemir <gozdemir_92@outlook.com>
2021-01-24 01:08:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day4csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
